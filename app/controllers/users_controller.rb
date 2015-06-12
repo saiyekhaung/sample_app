dd843caa113ee53d6314e3500d18502f1c2e46ae
saiyekhaung <saiyekhaung@diplomats.com>
2015-06-13 04:00:18 +0900

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
- before_action :correct_user,   only: [:edit, :update]
+  class UsersController < ApplicationController
+   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                         :following, :followers]
  before_action :admin_user,     only: :destroy
   def index
     @users = User.paginate(page: params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-<<<<<<< HEAD
   resources :microposts,          only: [:create, :destroy]
-=======
->>>>>>> user-microposts
+  resources :relationships,       only: [:create, :destroy]
 end
